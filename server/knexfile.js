85be9793faa85f834e8ad53f2cf1bf6b8a782e0c
Lena Venable <venablena@gmail.com>
2018-01-24 10:49:07 -0800

    create folders for backend

---
+++ server/knexfile.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/dayheart_dev'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  }
+
+}
