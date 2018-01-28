2ab671e40b659411ba6219ab7f0eeec56584f499
Lena Venable <venablena@gmail.com>
2018-01-27 19:26:26 -0800

    activate map click

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { GoogleAPIKey } from '../config'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 export class MapContainer extends Component {
-render() {
-    if (!this.props.loaded) {
-      return (<div>Loading...</div>)
-    }
-    return (
-      <Map google={this.props.google} zoom={14} onReady={this.fetchPlaces}>
-
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
-
-      </Map>
-    );
+  handleClick = (props, marker, e) => {
+    console.log("I'm clicking on a marker");
   }
+
+  render() {
+      if (!this.props.loaded) {
+        return (<div>Loading...</div>)
+      }
+      return (
+        <Map google={this.props.google} zoom={14} onReady={this.fetchPlaces}>
+
+          <Marker onClick={this.handleClick}
+                  name={'Current location'} />
+
+        </Map>
+      );
+    }
 }
 
 export default GoogleApiWrapper({
