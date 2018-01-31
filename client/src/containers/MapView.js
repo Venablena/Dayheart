51c13871e0ebf1ff625e5af2ba83cdbf505358e2
Lena Venable <venablena@gmail.com>
2018-01-30 17:30:57 -0800

    fromatting

--- client/src/containers/MapView.js
+++ client/src/containers/MapView.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Toolbar from '../components/Toolbar'
 import Infobar from '../components/Infobar'
+import Overlay from '../components/Overlay'
 import GoogleApiWrapper from '../components/Map'
+import { Container }from 'semantic-ui-react'
 
 const MapView = ({providers, user}) => {
     console.log('MapView props:', providers, user);
@@ -9,7 +11,10 @@ const MapView = ({providers, user}) => {
       <main>
         <Toolbar />
         <Infobar />
-        <GoogleApiWrapper providers= {providers} user={user}/>
+        <Container>
+          <GoogleApiWrapper providers= {providers} user={user}/>
+        <Overlay providers= {providers} user={user}/>
+        </Container>
       </main>
   )
 }
