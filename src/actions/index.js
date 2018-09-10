2d55addc7b9325d8779a583d20639197ce8a3194
Lena Venable <venablena@gmail.com>
2018-09-10 14:21:27 -0700

    provider fetch cache

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,22 @@
 import firebase from 'firebase'
 
-// export const PROVIDERS_FETCH_SUCCESS = 'PROVIDERS_FETCH_SUCCESS'
-// export const getProviders = () => (dispatch, getState) => {
-//   if (getState().dayheart.providers.length) {
-//     dispatch({
-//       type: PROVIDERS_FETCH_SUCCESS,
-//       payload: getState().dayheart.providers
-//     })
-//   } else {
-//     firebase.database().ref(`/providers`)
-//       .once('value', snapshot => {
-//         dispatch({
-//           type: PROVIDERS_FETCH_SUCCESS,
-//           payload: snapshot.val()
-//         })
-//     })
-//   }
-// }
-
+//only load providers if they are not already cached
 export const PROVIDERS_FETCH_SUCCESS = 'PROVIDERS_FETCH_SUCCESS'
-export const getProviders = () => (dispatch) => {
-  firebase.database().ref(`/providers`)
-    .once('value', snapshot => {
-      dispatch({
-        type: PROVIDERS_FETCH_SUCCESS,
-        payload: snapshot.val()
-      })
-  })
+export const getProviders = () => (dispatch, getState) => {
+  if (getState().dayheart.providers.length) {
+    dispatch({
+      type: PROVIDERS_FETCH_SUCCESS,
+      payload: getState().dayheart.providers
+    })
+  } else {
+    firebase.database().ref(`/providers`)
+      .once('value', snapshot => {
+        dispatch({
+          type: PROVIDERS_FETCH_SUCCESS,
+          payload: snapshot.val()
+        })
+    })
+  }
 }
 
 export const FAVORITES_FETCH_SUCCESS = 'FAVORITES_FETCH_SUCCESS'
